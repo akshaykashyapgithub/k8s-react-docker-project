3ce3f861e7807727d7d6f15afbf9c051f210a523
Akshay Kashyap <akshaykashyap7674@gmail.com>
2025-05-28 13:03:12 +0200

    Initial commit - React Docker Kubernetes CLI Project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my demo Project for Docker and Kubernetes 
+          This is the change in APP.js File Version:02
+          This Line for Version 3 To test it working live without intruption(ZeroDownTime)
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
